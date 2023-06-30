b8f9d87a69846bdd3dbb2444f8d20d654b0405d7
qwotic <qwoticreator@gmail.com>
2023-06-30 13:06:37 +0700

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Данил Белов on 30.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
